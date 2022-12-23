fabe86e3689ab899fc245852e1af9765ecf0412f
Glitch (grandiose-nosy-cloudberry) <none>
2022-12-23 15:43:36 +0000

    👱📈 Checkpoint ./src/config.js:52415439/144

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  "token": "MTA0ODg4MzI2NzEwNzAyOTAyMg.GJNXsC.sw5EmfTAOmRSvibuBAOvl-QKGEeiSWXLmVEVxA",
+  "token": "MTA0ODg4MzI2NzEwNzAyOTAyMg.GYeNxS.7z_UhYl0cXiVc-Fls0maU4wliRAwmiO4bSIel0",
   "language": true
 }
